f1329a93835b0bcaf5c78ddbd53aa0cb37018307
Ivan Zabrodin <zabrodinivan77@gmail.com>
2015-12-29 20:02:55 +0300

    add button up

--- js/script.js
+++ js/script.js
@@ -30,3 +30,19 @@ function formatText(index, panel) {
                 $('.anythingSlider').anythingSlider(6);
             });
         });
+
+  var top_show = 150; // В каком положении полосы прокрутки начинать показ кнопки "Наверх"
+  var delay = 1000; // Задержка прокрутки
+  $(document).ready(function() {
+    $(window).scroll(function () { // При прокрутке попадаем в эту функцию
+      /* В зависимости от положения полосы прокрукти и значения top_show, скрываем или открываем кнопку "Наверх" */
+      if ($(this).scrollTop() > top_show) $('#top').fadeIn();
+      else $('#top').fadeOut();
+    });
+    $('#top').click(function () { // При клике по кнопке "Наверх" попадаем в эту функцию
+      /* Плавная прокрутка наверх */
+      $('body, html').animate({
+        scrollTop: 0
+      }, delay);
+    });
+  });
